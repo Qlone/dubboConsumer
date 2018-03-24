232b44ba8a9c9722e79c0b5054cca6bee43065c7
heweinan <870603569@qq.com>
2018-03-24 11:19:39 +0800

    init

---
+++ src/main/java/com/qlone/consumer/controller/TestController.java
@@ -0,0 +1,34 @@
+package com.qlone.consumer.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.qlone.api.login.Test;
+import com.qlone.api.login.service.UserModelService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class TestController {
+    @Reference(version = "1.0.0")
+    private Test test;
+    @Reference(version = "1.0.0")
+    private UserModelService modelService;
+
+    @ResponseBody
+    @RequestMapping("")
+    public Object getBill(ModelAndView modelAndView){
+        modelAndView.addObject("res",test.getMessage());
+        return modelAndView.getModel();
+    }
+
+    @ResponseBody
+    @RequestMapping("/s")
+    public Object newUser(ModelAndView modelAndView){
+        String res = modelService.loginAndGetToken("test","password");
+
+        modelAndView.addObject("res",res);
+        return modelAndView.getModel();
+    }
+}
