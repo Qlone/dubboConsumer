30eae7c4b52185cf91d8835417d83d4b92588d0c
heweinan <870603569@qq.com>
2018-03-24 17:12:13 +0800

    1

--- src/main/java/com/qlone/consumer/controller/TestController.java
+++ src/main/java/com/qlone/consumer/controller/TestController.java
@@ -4,6 +4,7 @@ import com.alibaba.dubbo.config.annotation.Reference;
 import com.qlone.api.login.Test;
 import com.qlone.api.login.service.UserModelService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
@@ -27,6 +28,15 @@ public class TestController {
     @RequestMapping("/s")
     public Object newUser(ModelAndView modelAndView){
         String res = modelService.loginAndGetToken("test","password");
+
+        modelAndView.addObject("res",res);
+        return modelAndView.getModel();
+    }
+
+    @ResponseBody
+    @RequestMapping("/c")
+    public Object checkToken(ModelAndView modelAndView, @PathVariable String token){
+        String res = modelService.checkToken(token);
 
         modelAndView.addObject("res",res);
         return modelAndView.getModel();
