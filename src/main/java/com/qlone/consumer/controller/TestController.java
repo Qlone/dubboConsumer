03434491135f830e5dd6a142e3e14f2a34f697dc
heweinan <870603569@qq.com>
2018-03-26 18:10:21 +0800

    登陆token完成

--- src/main/java/com/qlone/consumer/controller/TestController.java
+++ src/main/java/com/qlone/consumer/controller/TestController.java
@@ -1,24 +1,25 @@
 package com.qlone.consumer.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.qlone.api.basic.enumapi.ApiResult;
 import com.qlone.api.login.Test;
 import com.qlone.api.login.service.UserModelService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("")
 public class TestController {
     @Reference(version = "1.0.0")
     private Test test;
     @Reference(version = "1.0.0")
-    private UserModelService modelService;
+    private UserModelService userModelService;
 
     @ResponseBody
-    @RequestMapping("")
+    @RequestMapping("/")
     public Object getBill(ModelAndView modelAndView){
         modelAndView.addObject("res",test.getMessage());
         return modelAndView.getModel();
@@ -26,8 +27,9 @@ public class TestController {
 
     @ResponseBody
     @RequestMapping("/s")
-    public Object newUser(ModelAndView modelAndView){
-        String res = modelService.loginAndGetToken("test","password");
+    public Object newUser(ModelAndView modelAndView,@RequestParam(value = "user") String user,
+                          @RequestParam(value = "psw") String psw){
+        String res = userModelService.loginAndGetToken(user,psw,false);
 
         modelAndView.addObject("res",res);
         return modelAndView.getModel();
@@ -35,8 +37,18 @@ public class TestController {
 
     @ResponseBody
     @RequestMapping("/c")
-    public Object checkToken(ModelAndView modelAndView, @PathVariable String token){
-        String res = modelService.checkToken(token);
+    public Object checkToken(ModelAndView modelAndView ,@RequestParam(value = "token") String token){
+        String res = userModelService.checkToken(token);
+
+        modelAndView.addObject("res",res);
+        return modelAndView.getModel();
+    }
+
+    @ResponseBody
+    @RequestMapping("/r")
+    public Object register(ModelAndView modelAndView ,@RequestParam(value = "user") String user,
+                           @RequestParam(value = "psw") String psw){
+        ApiResult res = userModelService.registerAccount(user,psw);
 
         modelAndView.addObject("res",res);
         return modelAndView.getModel();
